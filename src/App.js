a07b6dcfc38b1697be693fb6b81a7f345740de36
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 15:21:06 +0200

    Add posts rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ApiRequest from './api';
 
 const App = () => {
-  return <div>React</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await ApiRequest.getAll();
+      setPosts(response.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
