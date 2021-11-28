bdf9c923468e60909029c5bf5b638f01beb430ae
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 19:02:12 +0200

    Add post number to title

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,7 +4,7 @@ import Button from './button/Button';
 const PostItem = ({ post }) => {
   return (
     <div className="post" key={post.id}>
-      <h3 className="post__title">{post.title}</h3>
+      <h3 className="post__title">{post.id}. {post.title}</h3>
       <p className="post__body">{post.body}</p>
       <div className="post__buttons">
         <Button>View</Button>
