187585760fbb38106692694f1c15b9835e212157
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 19:25:38 +0200

    Add pagination feature

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import { useFetching } from './hooks/useFetching';
 import PostFilter from './components/PostFilter';
 import './styles/app.css';
 import PostList from './components/PostList';
+import Pagination from './components/pagination/Pagination';
+import { getPageCount } from './utils/pages';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await ApiRequest.getAll();
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const response = await ApiRequest.getAll(limit, page);
     setPosts(response.data);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   });
   const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetchPosts();
+    fetchPosts(limit, page);
   }, []);
 
   const filteredPosts = usePosts(posts, filter.sort, filter.query);
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts(limit, page);
+  };
 
   return (
     <div className="app">
@@ -34,6 +47,11 @@ const App = () => {
         ) : (
           <PostList posts={filteredPosts} />
         )}
+        <Pagination
+          page={page}
+          changePage={changePage}
+          totalPages={totalPages}
+        />
       </div>
     </div>
   );
