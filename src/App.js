08d76cdc35ef026eb5d1d4567e7f5c9feb234da0
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 17:55:55 +0200

    Implement useFetching for getting & handling posts

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import ApiRequest from './api';
 import Button from './components/button/Button';
 import Input from './components/input/Input';
 import Select from './components/select/Select';
+import Spinner from './components/spinner/Spinner';
 import { usePosts } from './hooks/usePosts';
+import { useFetching } from './hooks/useFetching';
 import './styles/app.css';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await ApiRequest.getAll();
+    setPosts(response.data);
+  });
   const [filter, setFilter] = useState({ sort: '', query: '' });
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await ApiRequest.getAll();
-      setPosts(response.data);
-    };
+
     fetchPosts();
   }, []);
 
@@ -41,18 +44,25 @@ const App = () => {
           />
         </div>
         <hr style={{ margin: '20px 0', width: '100%' }} />
-        <div className="posts">
-          {filteredPosts.map((post) => (
-            <div className="post" key={post.id}>
-              <h3 className="post__title">{post.title}</h3>
-              <p className="post__body">{post.body}</p>
-              <div className="post__buttons">
-                <Button>View</Button>
-                <Button>Delete</Button>
+        {postError && <h3>Error: {postError}</h3>}
+        {isPostsLoading ? (
+          <div className="spinner">
+            <Spinner />
+          </div>
+        ) : (
+          <div className="posts">
+            {filteredPosts.map((post) => (
+              <div className="post" key={post.id}>
+                <h3 className="post__title">{post.title}</h3>
+                <p className="post__body">{post.body}</p>
+                <div className="post__buttons">
+                  <Button>View</Button>
+                  <Button>Delete</Button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
