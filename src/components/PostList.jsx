603bbe669f124d2b7993b7b20f79aed5ea703ee7
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 19:00:31 +0200

    add check for empty posts array

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PostItem from './PostItem';
 
 const PostList = ({ posts }) => {
+  if (!posts.length) {
+    return <h2 style={{ textAlign: 'center', color: '#fff' }}>No posts</h2>;
+  }
+
   return (
     <div className="posts">
       {posts.map((post) => (
