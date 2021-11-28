5e815427635a75109e0677d677eb18f872edc9c7
Denys Bilonozhko <deformix@gmail.com>
2021-11-28 16:46:25 +0200

    Add button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ApiRequest from './api';
+import Button from './components/button/Button';
 import './styles/app.css';
 
 const App = () => {
@@ -15,14 +16,18 @@ const App = () => {
 
   return (
     <div className="app">
-     <div className="posts">
+      <div className="posts">
         {posts.map((post) => (
           <div className="post" key={post.id}>
             <h3 className="post__title">{post.title}</h3>
             <p className="post__body">{post.body}</p>
+            <div className="post__buttons">
+              <Button>View</Button>
+              <Button>Delete</Button>
+            </div>
           </div>
         ))}
-     </div>
+      </div>
     </div>
   );
 };
